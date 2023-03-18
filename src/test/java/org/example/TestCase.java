af35d5db5ca9162116db9e618905bc72a16be729
radik <rad.husainov2016@gmail.com>
2023-03-19 01:32:27 +0400

    edited pom.xml

--- src/test/java/org/example/TestCase.java
+++ src/test/java/org/example/TestCase.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import junit.framework.Assert;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,11 +19,9 @@ public class TestCase {
     /**
      * осуществление первоначальной настройки
      */
-    @BeforeClass
+    @BeforeAll
     public static void setup() {
-        //определение пути до драйвера и его настройка
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
-        //создание экземпляра драйвера
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
@@ -31,26 +29,18 @@ public class TestCase {
         cartPage = new CartPage(driver);
         inventoryPage = new InventoryPage(driver);
         checkoutPage = new CheckoutPage(driver);
-        //окно разворачивается на полный экран
         driver.manage().window().maximize();
-        //задержка на выполнение теста = 10 сек.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //получение ссылки на страницу входа из файла настроек
-        driver.get(ConfProperties.getProperty("loginpage"));
+        driver.get(ConfProperties.getProperty("login-page"));
     }
-    /**
-     * тестовый метод для осуществления аутентификации
-     */
     @Test
     public void loginTest() {
-        //получение доступа к методам класса LoginPage для взаимодействия с элементами страницы
-        //значение login/password берутся из файла настроек по аналогии с chromedriver
-        //и loginpage
-        //вводим логин
+
+
         loginPage.inputLogin(ConfProperties.getProperty("login"));
-        //вводим пароль
+
         loginPage.inputPassword(ConfProperties.getProperty("password"));
-        //нажимаем кнопку входа
+
         loginPage.clickLoginBtn();
 
         inventoryPage.addToCart();
@@ -58,9 +48,9 @@ public class TestCase {
 
         cartPage.pressCheckout();
 
-        checkoutPage.inputFirstName("test");
-        checkoutPage.inputLastName("test");
-        checkoutPage.inputZipCode("test");
+        checkoutPage.inputFirstName(ConfProperties.getProperty("first-name"));
+        checkoutPage.inputLastName(ConfProperties.getProperty("last-name"));
+        checkoutPage.inputZipCode(ConfProperties.getProperty("zip-code"));
 
         checkoutPage.pressContinue();
         checkoutPage.pressFinish();
@@ -68,19 +58,19 @@ public class TestCase {
         String url = checkoutPage.getUrl();
         String thanks = checkoutPage.getThankMessage();
 
-        Assert.assertEquals(ConfProperties.getProperty("successpage"), url);
-        Assert.assertEquals(ConfProperties.getProperty("thanks"), thanks);
+        Assertions.assertEquals(ConfProperties.getProperty("success-page"), url);
+        Assertions.assertEquals(ConfProperties.getProperty("thanks-message"), thanks);
 
     }
     @Test
-    public void invalidLogin() {
-        driver.get(ConfProperties.getProperty("loginpage"));
-        loginPage.inputLogin(ConfProperties.getProperty("invalidlogin"));
-        loginPage.inputPassword(ConfProperties.getProperty("invalidpassword"));
+    public void invalidLoginTest() {
+        driver.get(ConfProperties.getProperty("login-page"));
+        loginPage.inputLogin(ConfProperties.getProperty("invalid-login"));
+        loginPage.inputPassword(ConfProperties.getProperty("invalid-password"));
         loginPage.clickLoginBtn();
-        Assert.assertEquals(ConfProperties.getProperty("errormessage"), loginPage.getErrorMessage());
+        Assertions.assertEquals(ConfProperties.getProperty("error-message"), loginPage.getErrorMessage());
     }
-    @AfterClass
+    @AfterAll
     public static void tearDown() {
         driver.quit();
     }
